c807d7947999a8061b428395fee2b81468aa14e6
Patrick Dugan <patrickdugan04@gmail.com>
2020-06-13 15:49:41 -0400

    added initial commit

---
+++ Server/index.php
@@ -0,0 +1,51 @@
+<html>
+  <head class="grey">
+    <!--run with php -S ip:port-->
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <!--Import materialize.css-->
+     <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+     <link type="text/css" rel="stylesheet" href="extra.css"  media="screen,projection"/>
+     <!--Let browser know website is optimized for mobile-->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+   </head>
+   <body class="black">
+     <div class="section grey">
+       <h2 class="center-align">Code Guessing Game</h5>
+       <h5 class="center-align">yay</h5>
+     </div>
+      <!--button row-->
+      <br>
+      <div class="row">
+            <div class="col s1"></div>
+
+            <div class="col s2">
+              <div class="row">
+                <a class="waves-effect waves-light btn">Login</a>
+              </div>
+
+              <div class="row">
+                <a class="waves-effect waves-light btn">Join</a>
+              </div>
+            </div>
+
+            <div class="col s5">
+              <div class="rainB">
+                Text will go here
+              </div>
+            </div>
+
+            <div class="col s3">
+              <div class="collection">
+                <h4 class="collection-item">Players</h4>
+                <a href="#!" class="collection-item">Alvin</a>
+                <!--TODO write php to list players-->
+              </div>
+            </div>
+
+        </div>
+
+
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+   </body>
+</html>
